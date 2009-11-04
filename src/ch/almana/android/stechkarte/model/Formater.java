93939ae260872ca0772722295a51b7fe0102b838
vogtp <>
2009-11-04 08:53:18 +0100

    print leading 0 in minutes

--- src/ch/almana/android/stechkarte/model/Formater.java
+++ src/ch/almana/android/stechkarte/model/Formater.java
@@ -5,7 +5,7 @@ public class Formater {
 	public static CharSequence formatHourMinFromHours(float hours) {
 		int h = (int) Math.floor(hours);
 		int m = Math.round((hours - h) * 60); // rather: /100 * 60 * 100
-		return h + ":" + m;
+		return h + ":" + String.format("%02d", m);
 	}
 
 }
