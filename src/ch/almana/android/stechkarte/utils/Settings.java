87f810eff1e76c55286e745da8a968b76c04ce80
vogtp <>
2010-05-04 22:27:22 +0200

    split settings

--- src/ch/almana/android/stechkarte/utils/Settings.java
+++ src/ch/almana/android/stechkarte/utils/Settings.java
@@ -1,85 +1,24 @@
 package ch.almana.android.stechkarte.utils;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.util.Log;
 import ch.almana.android.stechkarte.R;
 import ch.almana.android.stechkarte.log.Logger;
 
-public class Settings {
-
+public class Settings extends SettingsBase {
+	
 	private static float hoursTargetDefault = 8.4f;
-
-	private static Settings instance;
-	private Context context;
-
-	private Settings(Context ctx) {
-		this.context = ctx;
-	}
-
+	
 	public static void initInstance(Context ctx) {
 		if (instance == null) {
 			instance = new Settings(ctx);
 		}
 	}
-
-	public static Settings getInstance() {
-		return instance;
+	
+	private Settings(Context ctx) {
+		super(ctx);
 	}
-
-	private SharedPreferences getPreferences() {
-		return PreferenceManager.getDefaultSharedPreferences(context);
-	}
-
-	private int getPrefAsInt(int key, int defaultValueKey) throws Exception {
-		String prefValue = getPrefAsString(key, defaultValueKey);
-		try {
-			return Integer.parseInt(prefValue);
-		} catch (Exception e) {
-			throw new Exception();
-		}
-	}
-
-	private float getPrefAsFloat(int key, int defaultValueKey) throws Exception {
-		String prefValue = getPrefAsString(key, defaultValueKey);
-		try {
-			return Float.parseFloat(prefValue);
-		} catch (Exception e) {
-			throw new Exception();
-		}
-	}
-
-	private long getPrefAsLong(int key, int defaultValueKey) throws Exception {
-		String prefValue = getPrefAsString(key, defaultValueKey);
-		try {
-			return Long.parseLong(prefValue);
-		} catch (Exception e) {
-			throw new Exception();
-		}
-	}
-
-	private String getPrefAsString(int key, int defaultValueKey) {
-		String defaultValue = context.getResources().getString(defaultValueKey);
-		String prefKey = context.getResources().getString(key);
-		return getPreferences().getString(prefKey, defaultValue);
-	}
-
-	private boolean getPrefAsBoolean(int key, int defaultValueKey) throws Exception {
-		String defaultValueString = context.getResources().getString(defaultValueKey);
-		boolean defaultValue;
-		if ("true".equalsIgnoreCase(defaultValueString)) {
-			defaultValue = true;
-		} else if ("false".equalsIgnoreCase(defaultValueString)) {
-			defaultValue = false;
-		} else {
-			throw new Exception();
-		}
-		String prefKey = context.getResources().getString(key);
-		return getPreferences().getBoolean(prefKey, defaultValue);
-	}
-
-
+	
 	public float getHoursTarget() {
 		try {
 			return getPrefAsFloat(R.string.prefKeyHoursPerDay, R.string.prefHoursPerDayDefault);
@@ -87,12 +26,12 @@ public class Settings {
 			Log.w(Logger.LOG_TAG, "Error parsing setting hours per day", e);
 			return hoursTargetDefault;
 		}
-
+		
 	}
-
+	
 	public boolean isFreeVersion() {
 		String lic = getPrefAsString(R.string.prefKeyLicence, R.string.prefLicenceDefault);
 		return !"sonnenscheinInBasel".equals(lic);
 	}
-
+	
 }
---
+++ src/ch/almana/android/stechkarte/utils/SettingsBase.java
@@ -0,0 +1,76 @@
+package ch.almana.android.stechkarte.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public abstract class SettingsBase {
+	
+	protected static Settings instance;
+	protected final Context context;
+	
+	public static void initInstance(Context ctx) {
+		
+	}
+	
+	public static Settings getInstance() {
+		return instance;
+	}
+	
+	public SettingsBase(Context ctx) {
+		super();
+		this.context = ctx;
+	}
+	
+	private SharedPreferences getPreferences() {
+		return PreferenceManager.getDefaultSharedPreferences(context);
+	}
+	
+	protected int getPrefAsInt(int key, int defaultValueKey) throws Exception {
+		String prefValue = getPrefAsString(key, defaultValueKey);
+		try {
+			return Integer.parseInt(prefValue);
+		} catch (Exception e) {
+			throw new Exception();
+		}
+	}
+	
+	protected float getPrefAsFloat(int key, int defaultValueKey) throws Exception {
+		String prefValue = getPrefAsString(key, defaultValueKey);
+		try {
+			return Float.parseFloat(prefValue);
+		} catch (Exception e) {
+			throw new Exception();
+		}
+	}
+	
+	protected long getPrefAsLong(int key, int defaultValueKey) throws Exception {
+		String prefValue = getPrefAsString(key, defaultValueKey);
+		try {
+			return Long.parseLong(prefValue);
+		} catch (Exception e) {
+			throw new Exception();
+		}
+	}
+	
+	protected String getPrefAsString(int key, int defaultValueKey) {
+		String defaultValue = context.getResources().getString(defaultValueKey);
+		String prefKey = context.getResources().getString(key);
+		return getPreferences().getString(prefKey, defaultValue);
+	}
+	
+	protected boolean getPrefAsBoolean(int key, int defaultValueKey) throws Exception {
+		String defaultValueString = context.getResources().getString(defaultValueKey);
+		boolean defaultValue;
+		if ("true".equalsIgnoreCase(defaultValueString)) {
+			defaultValue = true;
+		} else if ("false".equalsIgnoreCase(defaultValueString)) {
+			defaultValue = false;
+		} else {
+			throw new Exception();
+		}
+		String prefKey = context.getResources().getString(key);
+		return getPreferences().getBoolean(prefKey, defaultValue);
+	}
+	
+}
