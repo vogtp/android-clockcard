9148859b684ca986679e1657ade57a77ee3e8919
vogtp <>
2010-05-25 23:45:12 +0200

    install lic

--- src/ch/almana/android/stechkarte/view/StechkartePreferenceActivity.java
+++ src/ch/almana/android/stechkarte/view/StechkartePreferenceActivity.java
@@ -3,15 +3,29 @@ package ch.almana.android.stechkarte.view;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.Preference.OnPreferenceClickListener;
 import ch.almana.android.stechkarte.R;
 import ch.almana.android.stechkarte.utils.Settings;
 
 public class StechkartePreferenceActivity extends PreferenceActivity {
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+
+		Preference buyPreference = findPreference(getString(R.string.prefKeyBuy));
+		buyPreference.setEnabled(!Settings.getInstance().isPayVersion());
+		buyPreference
+				.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+					@Override
+					public boolean onPreferenceClick(Preference preference) {
+						BuyFullVersion
+								.startClockCardInstall(StechkartePreferenceActivity.this);
+						return true;
+					}
+				});
 
 		Preference betaFeatures = findPreference(getString(R.string.prefKeyLicence));
 		betaFeatures.setEnabled(Settings.getInstance().hasBetaFeatures());
@@ -20,5 +34,5 @@ public class StechkartePreferenceActivity extends PreferenceActivity {
 		emailPreference.setEnabled(Settings.getInstance()
 				.isEmailExportEnabled());
 	}
-	
+
 }
