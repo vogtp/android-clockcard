87bcfbec7fc27b1d8b8a33934c3c6d8217fb18a9
vogtp <>
2010-07-10 10:28:09 +0200

    set hours target on day change

--- src/ch/almana/android/stechkarte/view/DayEditor.java
+++ src/ch/almana/android/stechkarte/view/DayEditor.java
@@ -41,6 +41,7 @@ import ch.almana.android.stechkarte.provider.db.DB.Timestamps;
 import ch.almana.android.stechkarte.utils.DeleteDayDialog;
 import ch.almana.android.stechkarte.utils.DialogCallback;
 import ch.almana.android.stechkarte.utils.Formater;
+import ch.almana.android.stechkarte.utils.Settings;
 
 public class DayEditor extends ListActivity implements DialogCallback {
 
@@ -57,8 +58,6 @@ public class DayEditor extends ListActivity implements DialogCallback {
 	private ListView timestamps;
 	private SimpleCursorAdapter adapter;
 	private boolean overtimeAction;
-
-	// change work time on day change
 
 	/** Called when the activity is first created. */
 	@Override
@@ -207,6 +206,11 @@ public class DayEditor extends ListActivity implements DialogCallback {
 		fixed.setChecked(day.isFixed());
 		hoursTarget.setText(Formater.formatHourMinFromHours(day.getHoursTarget()));
 		hoursWorked.setText(Formater.formatHourMinFromHours(day.getHoursWorked()));
+		if (!day.isFixed()) {
+			// change work time on day change
+			hoursTarget.setText(Settings.getInstance().getHoursTarget(day.getDayRef()) + "");
+		}
+
 	}
 
 	private void updateModel() {
