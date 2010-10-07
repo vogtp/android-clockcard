25fcf55b74c3a6fc27927fe12bd53c56b204ffd2
vogtp <>
2010-10-07 21:18:53 +0200

    v 1.6.0

--- src/ch/almana/android/stechkarte/provider/StechkarteProvider.java
+++ src/ch/almana/android/stechkarte/provider/StechkarteProvider.java
@@ -5,30 +5,30 @@ import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
-import ch.almana.android.stechkarte.provider.db.DBBackendDay;
-import ch.almana.android.stechkarte.provider.db.DBBackendTimestamp;
 import ch.almana.android.stechkarte.provider.db.DB.Days;
 import ch.almana.android.stechkarte.provider.db.DB.OpenHelper;
 import ch.almana.android.stechkarte.provider.db.DB.Timestamps;
+import ch.almana.android.stechkarte.provider.db.DBBackendDay;
+import ch.almana.android.stechkarte.provider.db.DBBackendTimestamp;
 import ch.almana.android.stechkarte.view.appwidget.StechkarteAppwidget;
 
 public class StechkarteProvider extends ContentProvider {
-	
+
 	public static final String AUTHORITY = "ch.almana.android.stechkarte";
-	
+
 	private static final int TIMESTAMP = 1;
 	private static final int DAY = 2;
-	
+
 	private static final UriMatcher sUriMatcher;
-	
+
 	private OpenHelper openHelper;
-	
+
 	@Override
 	public boolean onCreate() {
 		openHelper = new OpenHelper(getContext());
 		return true;
 	}
-	
+
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		int count = 0;
@@ -42,24 +42,24 @@ public class StechkarteProvider extends ContentProvider {
 		default:
 			throw new IllegalArgumentException("Unknown URI " + uri);
 		}
-		
+
 		notifyChange(uri);
 		return count;
 	}
-	
+
 	@Override
 	public String getType(Uri uri) {
 		switch (sUriMatcher.match(uri)) {
 		case TIMESTAMP:
 			return DBBackendTimestamp.getType(uri);
-			
+
 		case DAY:
 			return DBBackendDay.getType(uri);
 		default:
 			throw new IllegalArgumentException("Unknown URI " + uri);
 		}
 	}
-	
+
 	@Override
 	public Uri insert(Uri uri, ContentValues initialValues) {
 		Uri ret;
@@ -73,11 +73,11 @@ public class StechkarteProvider extends ContentProvider {
 		default:
 			throw new IllegalArgumentException("Unknown URI " + uri);
 		}
-		
+
 		notifyChange(uri);
 		return ret;
 	}
-	
+
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
 		Cursor c;
@@ -91,13 +91,13 @@ public class StechkarteProvider extends ContentProvider {
 		default:
 			throw new IllegalArgumentException("Unknown URI " + uri);
 		}
-		
+
 		// Tell the cursor what uri to watch, so it knows when its source data
 		// changes
 		c.setNotificationUri(getContext().getContentResolver(), uri);
 		return c;
 	}
-	
+
 	@Override
 	public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
 		int count = 0;
@@ -111,23 +111,24 @@ public class StechkarteProvider extends ContentProvider {
 		default:
 			throw new IllegalArgumentException("Unknown URI " + uri);
 		}
-		
+
 		notifyChange(uri);
 		return count;
 	}
-	
+
 	private void notifyChange(Uri uri) {
 		getContext().getContentResolver().notifyChange(uri, null);
 		StechkarteAppwidget.updateView(getContext());
+		// StechkarteBackupAgentHelper.dataChanged();
 	}
-	
+
 	static {
 		sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 		sUriMatcher.addURI(AUTHORITY, Timestamps.CONTENT_ITEM_NAME, TIMESTAMP);
 		sUriMatcher.addURI(AUTHORITY, Timestamps.CONTENT_ITEM_NAME + "/#", TIMESTAMP);
 		sUriMatcher.addURI(AUTHORITY, Days.CONTENT_ITEM_NAME, DAY);
 		sUriMatcher.addURI(AUTHORITY, Days.CONTENT_ITEM_NAME + "/#", DAY);
-		
+
 	}
-	
+
 }
