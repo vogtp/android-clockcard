9a317a5b6d1c3a90c5fb00321b869edd6a112d90
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-02-21 21:07:14 +0100

    preferences rework/reorg

--- src/ch/almana/android/stechkarte/view/activity/StechkartePreferenceActivity.java
+++ src/ch/almana/android/stechkarte/view/activity/StechkartePreferenceActivity.java
@@ -1,10 +1,13 @@
 package ch.almana.android.stechkarte.view.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import ch.almana.android.stechkarte.R;
+import ch.almana.android.stechkarte.utils.DialogHelper;
 import ch.almana.android.stechkarte.utils.Settings;
 import ch.almana.android.stechkarte.utils.StechkarteBackupAgentHelper;
 
@@ -18,23 +21,15 @@ public class StechkartePreferenceActivity extends PreferenceActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
-		if (Settings.getInstance().hasHoloTheme()) {
+		Settings settings = Settings.getInstance();
+		if (settings.hasHoloTheme()) {
 			getActionBar().setSubtitle(R.string.preferencesTitle);
 		} else {
 			setTitle(getString(R.string.app_name) + ": " + getString(R.string.preferencesTitle));
 		}
 
-		Preference buyPreference = findPreference(getString(R.string.prefKeyBuy));
-		boolean payVersion = Settings.getInstance().isPayVersion();
-		buyPreference.setEnabled(!payVersion);
-		buyPreference.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+		boolean payVersion = settings.isPayVersion();
 
-			@Override
-			public boolean onPreferenceClick(Preference preference) {
-				BuyFullVersion.startClockCardInstall(StechkartePreferenceActivity.this);
-				return true;
-			}
-		});
 
 		Preference payRateOvertimePreference = findPreference(getString(R.string.prefKeyPaymentOvertime));
 		payRateOvertimePreference.setEnabled(payVersion);
@@ -45,11 +40,48 @@ public class StechkartePreferenceActivity extends PreferenceActivity {
 			payRatePreference.setSummary(R.string.prefPaymentLong);
 		}
 
-		boolean emailExportEnabled = Settings.getInstance().isEmailExportEnabled();
+		boolean emailExportEnabled = settings.isEmailExportEnabled();
 		Preference emailPreference = findPreference(getString(R.string.prefKeyEmailAddress));
 		emailPreference.setEnabled(emailExportEnabled);
-		findPreference(getString(R.string.prefKeyDecimalFormat)).setEnabled(false);
 		findPreference(getString(R.string.prefKeyCsvFieldSeparator)).setEnabled(emailExportEnabled);
+
+		StringBuffer versionSB = new StringBuffer();
+		versionSB.append(getString(R.string.label_version)).append(" ").append(settings.getVersionName());
+		findPreference("prefKeyVersion").setTitle(versionSB.toString());
+		findPreference("prefKeyChangelog").setOnPreferenceClickListener(new OnPreferenceClickListener() {
+
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Intent i = new Intent(StechkartePreferenceActivity.this, ChangelogActivity.class);
+				startActivity(i);
+				return true;
+			}
+		});
+
+		findPreference("prefKeyVersion").setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				Intent i = new Intent(Intent.ACTION_VIEW);
+				Uri fromParts = Uri.parse("market://search?q=pname:" + getPackageName());
+				i.setData(fromParts);
+				startActivity(i);
+				return true;
+			}
+		});
+
+		findPreference("prefKeyBackupRestore").setEnabled(settings.isBackupEnabled());
+		findPreference("prefKeyRestore").setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			@Override
+			public boolean onPreferenceClick(Preference preference) {
+				if (Settings.getInstance().isBackupEnabled()) {
+					startActivity(new Intent(StechkartePreferenceActivity.this, BackupRestoreActivity.class));
+				} else {
+					DialogHelper.showFreeVersionDialog(StechkartePreferenceActivity.this);
+				}
+				return true;
+			}
+		});
+		
 	}
 
 	@Override
